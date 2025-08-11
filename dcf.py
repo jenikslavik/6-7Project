2a432b0a131df7ad8f66bf1cdbe0f3299aec7a72
jenikslavik <janslavik311@gmail.com>
2025-08-11 22:35:50 +0200

    #

--- dcf.py
+++ dcf.py
@@ -7,18 +7,43 @@ from google.oauth2.service_account import Credentials
 
 
 def get_companyData(ticker):
-    with open('/home/mo-lester/Documents/6-7 Project/company_tickers.json', 'r') as f:
+    with open('/home/mo-lester/Documents/6-7 Project/company_tickers.json', 'r') as f: # get cik based on ticker from company_tickers.json
         data = json.load(f)
     
     for v in data.values():
         if v['ticker'] == ticker:
-            cik = str(v['cik_str']).zfill(10)
+            cik = str(v['cik_str']).zfill(10) # format the ticker so that it has 10 digits (needed for the url)
     
-    return requests.get(f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json', headers={'User-Agent': 'janslavik311@gmail.com'}).json()
+    return requests.get(f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json', headers={'User-Agent': 'janslavik311@gmail.com'}).json() # get the json data file for the company (https://data.sec.gov/api/xbrl/companyfacts/CIK0001652044.json try this link for google)
 
 
-def calculate_ttm_interest_expense(ticker):
-    companyData = get_companyData(ticker)
+def calculate_ttm_interest_expense(ticker): # we need to calculate ttm interest expense
+    companyData = get_companyData(ticker) # call the json data file
+
+    '''this is an output for the loop for entry in companyData['facts']['us-gaap']['InterestExpenseNonoperating']['units']['USD'] (this is just shorter version, theres smthing around 100 of these blocks)
+        we need to get the "val" figure, q1-q3 are being reported just fine, they report the val (value) for the 3 months but for q4, they only report the value for the whole year, no 3 month value, so we need to subtract the ytd q3 figure (9 months) from the q4 (12 months) figure
+    {
+        "start": "2022-01-01",
+        "end": "2022-12-31",
+        "val": 357000000,
+        "accn": "0001652044-25-000014",
+        "fy": 2024,
+        "fp": "FY",
+        "form": "10-K",
+        "filed": "2025-02-05",
+        "frame": "CY2022"
+    },
+    {
+        "start": "2023-01-01",
+        "end": "2023-06-30",
+        "val": 123000000,
+        "accn": "0001652044-24-000079",
+        "fy": 2024,
+        "fp": "Q2",
+        "form": "10-Q",
+        "filed": "2024-07-24"
+    },
+    '''
 
     rows = []
     q4_rows = []
@@ -116,20 +141,24 @@ def get_metrics_to_calculate_fcf(ticker, metric):
     df = pd.DataFrame(rows)
     df['filed'] = pd.to_datetime(df.get('filed', df.get('end')))
     df = df.sort_values('filed', ascending=False)
-    df = df.drop_duplicates(['start', 'end'], keep='first').sort_values('end', ascending=True).drop(columns=['start', 'end', 'filed'])
+    df = df.drop_duplicates(['start', 'end'], keep='first').sort_values('end', ascending=True).drop(columns=['start', 'filed'])
+
+    df = df.rename(columns={'val': metric})
 
     rows = []
     for pos in range(len(df)):
         if df['fp'].iloc[pos] == 'Q1':
             row = {
                 'fp': df['fp'].iloc[pos],
-                'val': df['val'].iloc[pos]
+                'end': df['end'].iloc[pos],
+                metric: df[metric].iloc[pos]
             }
         else:
-            val = df['val'].iloc[pos] - df['val'].iloc[pos-1]
+            val = df[metric].iloc[pos] - df[metric].iloc[pos-1]
             row = {
                 'fp': 'Q4' if df['fp'].iloc[pos] == 'FY' else df['fp'].iloc[pos],
-                'val': val
+                'end': df['end'].iloc[pos],
+                metric: val
             }
         rows.append(row)
 
@@ -143,7 +172,7 @@ def get_and_plot_fcf(ticker):
 
     df = pd.DataFrame()
     for metric in metrics:
-        df = pd.concat([df, get_metrics_to_calculate_fcf(ticker, metric)], ignore_index=True)
+        df = pd.merge(df, get_metrics_to_calculate_fcf(ticker, metric), on=['fp'], how='outer')
 
     print(df)
 
